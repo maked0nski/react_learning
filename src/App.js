b1f630c892567f5ee2b9f7c8e82dcceeead1f779
maked0nski <maked0nskihtc@gmail.com>
2022-01-18 21:13:03 +0200

    home_work_react3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import './App.css';
+import { render } from "react-dom";
+import {
+    Link,
+    Routes,
+    Route
+} from "react-router-dom";
+
+import css from './App.module.css';
+import HomePage from "./pages/HomePage/HomePage";
+import PostsPage from "./pages/PostsPage/PostsPage";
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
+import UserPage from "./pages/UsersPage/UserPage";
 
 function App() {
-  return (
-    <>
+    return (
+        <>
+            <div className={css.header}>
+                <Link to='/'>Home</Link>
+                <Link to='users'>Users</Link>
+                <Link to='posts'>Posts</Link>
+            </div>
 
-    </>
-  );
+            <Routes>
+                <Route path={"/"} element={<HomePage/>}/>
+                <Route path={"users"} element={<UserPage/>}/>
+                <Route path={"posts"} element={<PostsPage/>}/>
+            </Routes>
+        </>
+
+
+    );
 }
 
 export default App;
