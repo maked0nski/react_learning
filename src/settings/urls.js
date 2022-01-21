1c4652f0237bf14b7918554cd4c0d5e4d80f7ae7
maked0nski <maked0nskihtc@gmail.com>
2022-01-21 23:46:02 +0200

    старт

---
+++ src/settings/urls.js
@@ -0,0 +1,11 @@
+const baseURL = 'https://loremflickr.com/320/240';
+
+export default baseURL;
+
+export const urls = {
+    cat:'/cat',
+    car:'car',
+    dog:'/dog',
+    girl:'girl',
+    notebook:'notebook'
+}
