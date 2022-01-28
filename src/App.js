e74d7599bc89d697d9d0e2dca0480d4d5a1835ed
maked0nski <maked0nskihtc@gmail.com>
2022-01-28 22:15:17 +0200

    hw_useReducer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
+import {useReducer} from "react"
+
+const reduser = (state, action) => {
+  console.log(action);
+  console.log(state);
+  return state
+
+}
 
 function App() {
-  return (
-    <>
 
-    </>
+  const [state, dispatch] = useReducer(reduser, {count1:0});
+
+
+  return (
+    <div>
+<div>{state.count1}</div>
+      <button onClick={click}>Click</button>
+    </div>
   );
 }
 
